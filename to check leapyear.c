cd95a7a5438b26b0132a1aa251af1e105b31f414
sunilkumarbysani <51699442+sunilkumarbysani@users.noreply.github.com>
2019-07-16 09:37:57 +0530

    Create to check leapyear.c

---
+++ to check leapyear.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+ {
+   int n;
+   scanf("%d",&n);
+   if(n%4==0)
+   {
+     if(n%100==0)
+     {
+       if(n%400==0)
+       printf("yes");
+     }
+     else
+     printf("no");
+   }
+  else
+  printf("no");
+  return 0;
+}
